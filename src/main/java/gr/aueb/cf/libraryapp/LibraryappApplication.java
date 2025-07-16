74928abf3d67a7c8b82fe110a994e4d6dc1f7be2
Alexandros Polymeros <alpolymer@gmail.com>
2025-07-16 19:46:20 +0300

    feat add AbstractEntity Class

--- src/main/java/gr/aueb/cf/libraryapp/LibraryappApplication.java
+++ src/main/java/gr/aueb/cf/libraryapp/LibraryappApplication.java
@@ -2,8 +2,10 @@ package gr.aueb.cf.libraryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing//Spring Data provides support to keep track of who created an entity, when it got created, when the update happened and by whom.
 public class LibraryappApplication {
 
 	public static void main(String[] args) {
